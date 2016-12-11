1e23fd706915e80103a89269a1f9e77ae73381b7
zcj <fresh888888@gmail.com>
2016-12-11 20:24:41 +0800

    Initial commit

---
+++ src/main/java/cn/umbrella/intelli/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.umbrella.intelli;
+
+/**
+ * Created by zcj on 16/12/11.
+ */
+public class HelloWorld {
+    public static void main(){
+        System.out.println("Hello world !!!");
+    }
+}
