ae8207bbe11660f47fabc6c653af0384268f9811
zcj <fresh888888@gmail.com>
2016-12-11 20:36:49 +0800

    add a message

--- src/main/java/cn/umbrella/intelli/HelloWorld.java
+++ src/main/java/cn/umbrella/intelli/HelloWorld.java
@@ -6,5 +6,6 @@ package cn.umbrella.intelli;
 public class HelloWorld {
     public static void main(){
         System.out.println("Hello world !!!");
+        System.out.print("Demo Intelli Ide");
     }
 }
