6ec9bd040dbc67473e898968920eeff9819b462d
zcj <fresh888888@gmail.com>
2017-01-08 11:17:53 +0800

    add a  junit test case

--- src/main/java/cn/umbrella/intelli/HelloWorld.java
+++ src/main/java/cn/umbrella/intelli/HelloWorld.java
@@ -1,9 +1,50 @@
 package cn.umbrella.intelli;
 
 
-public class HelloWorld {
-    public static void main(){
-        System.out.println("Hello world !!!");
-        System.out.print("Demo Intelli Ide");
+    public class HelloWorld {
+        public static void main(String[] args) {
+            Thread10_Entity entity = new Thread10_Entity();
+            Thread10_1 t10_1 = new  Thread10_1(entity);
+            t10_1.start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println(entity.get());
+
+        }
     }
-}
+
+    class Thread10_1 extends Thread {
+        private Thread10_Entity thread10_Entity;
+
+        public Thread10_1(Thread10_Entity t10) {
+            this.thread10_Entity = t10;
+        }
+        @Override
+        public void run() {
+            thread10_Entity.set("admin", "admin");
+        }
+    }
+    class Thread10_Entity {
+        private String username = "user";
+        private String password = "user";
+
+        synchronized void set(String username, String password) {
+            this.username = username;
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            this.password = password;
+        }
+
+        String get() {
+            return username + "   " + password;
+        }
+    }
+
